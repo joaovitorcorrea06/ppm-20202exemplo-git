b7af13ea3e3e53b42421cf06bc32af8bd06a01aa
joaovitorcorrea06 <70239873+joaovitorcorrea06@users.noreply.github.com>
2020-09-01 21:42:12 -0300

    Adicionaei o nome

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 var botoes = document.getElementsByTagName('button')
-botoes [0].addEventListener('click', function(){
+
+botoes [0].addEventListener('click', bemVindo);
+
+function bemVindo(){
     window.alert('seja bem vindo a minha primeira pagina web')
-});
+}
