7f420a4ea8c1811a5b2ef63f6c388ec8397db42f
joaovitorcorrea06 <70239873+joaovitorcorrea06@users.noreply.github.com>
2020-09-08 21:35:35 -0300

    tipos primitivos

---
+++ js exemplos/script.js
@@ -0,0 +1,72 @@
+var booleana = true;
+console.log(booleana);
+
+booleana = false;
+console.log(booleana);
+
+console.log(typeof(booleana));
+
+var numero = 1;
+console.log(numero);
+console.log(typeof(numero));
+
+numero = Math.PI;
+console.log(numero);
+console.log(typeof(numero));
+/*
+var texto = 'texto';
+console.log(texto);
+console.log(typeof(texto));
+
+var texto = "texto";
+console.log(texto);
+console.log(typeof(texto));
+*/
+var n1= 10;
+var n2= 20;
+
+console.log('soma: ', n1 + n2);
+
+console.log('soma: ', n1 - n2);
+
+console.log('soma: ', n1 * n2);
+
+console.log('soma: ', n1 / n2);
+
+console.log('Potencia n2 elevado a n2: ', Math.pow(2,5));
+console.log('raiz quadrada de 25: ', Math,sqrt(25));
+
+var pi = Math.PI;
+var numeroRandomico = Math.random()*100;
+
+console.log('numero randomico'): numeroRandomico;
+console.log('numero randomico'): Math.floor(numeroRandomico); 
+console.log('numero randomico'): Math.trunc (numeroRandomico);
+console.log('numero randomico'): Math.round(numeroRandomico);
+//------------------------
+
+var texto = 'texto';
+console.log(texto);
+console.log(typeof(texto));
+
+var texto = 'c';
+console.log(texto);
+console.log(typeof(texto));
+
+var texto = "texto";
+console.log(texto);
+console.log(typeof(texto));
+
+console.log(texto.toUpperCase);
+console.log(texto.toLocaleLowerCase);
+
+var texto = 'texto';
+console.log(texto.startsWith('tex'));
+console.log((texto.endsWith('to')));
+console.log((texto.search('x')));
+console.log((texto.indexOf('to')));
+
+console.log((texto.includes('x')));
+
+console.log((texto.charAt('x')));
+
